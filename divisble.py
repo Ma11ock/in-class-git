abb845e64dead22267ab2e96d554b6279661355b
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-04-15 17:10:41 -0700

    finds divisors for an integer

--- divisble.py
+++ divisble.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 
 # List all the possible divisors for given number
+natNum = int(input("Give an integer: "))
 
+for i in range(1, natNum + 1):
+    if natNum % i == 0:
+        print(i)
