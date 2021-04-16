82342e8dec4087b0534a538dd9a112119b69edd8
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-04-15 17:01:06 -0700

    random string generator

--- passgen.py
+++ passgen.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 
+import string
+import random
+
 # Generates a password from a length given by a user
+
+letters = string.ascii_letters
+print ( ''.join(random.choice(letters) for i in range(10)) )
