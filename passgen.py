0fc9139aa372f02478660a046dcc90c250218e48
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-04-15 17:04:43 -0700

    get user chosen length

--- passgen.py
+++ passgen.py
@@ -4,6 +4,7 @@ import string
 import random
 
 # Generates a password from a length given by a user
-
 letters = string.ascii_letters
-print ( ''.join(random.choice(letters) for i in range(10)) )
+print(''.join(random.choice(letters) for i in range(int(input("How long should the password be? ")))))
+
+
